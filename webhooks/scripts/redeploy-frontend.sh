a1de8732bc9d24715e102de4341e8ef860927780
Anton Orlov <orlov88@gmail.com>
2023-03-09 16:37:06 +0300

    Правка вебхуков

--- webhooks/scripts/redeploy-frontend.sh
+++ webhooks/scripts/redeploy-frontend.sh
@@ -2,4 +2,4 @@
 docker pull ghcr.io/itrolegames/hat-frontend:master
 docker stop hat-frontend
 docker system prune -f
-docker run -d -p 80:80 --name=hat-frontend ghcr.io/itrolegames/hat-frontend:master
+docker run -d -p 80:80 --name=hat-frontend ghcr.io/itrolegames/hat-frontend:latest
--- webhooks/scripts/redeploy-user.sh
+++ webhooks/scripts/redeploy-user.sh
@@ -1,5 +1,5 @@
 #!bin/sh
-docker pull ghcr.io/itrolegames/hat-user-back:master
+docker pull ghcr.io/itrolegames/hat-user-back:latest
 docker stop hat-user-back
 docker system prune -f
 docker run -d -p 9001:9001 --env APP_HOST=172.17.0.1 --env DB_PORT=5401 --env DB_USER=hat_user --env DB_PASSWORD=test1234 --name=hat-user-back ghcr.io/itrolegames/hat-user-back:master
