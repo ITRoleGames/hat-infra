9f30cdd018f757c0a924349926a43d2e2b907d50
Anton Orlov <orlov88@gmail.com>
2023-03-09 17:17:37 +0300

    Правка вебхуков

--- webhooks/scripts/redeploy-api-gateway.sh
+++ webhooks/scripts/redeploy-api-gateway.sh
@@ -2,4 +2,4 @@
 docker pull ghcr.io/itrolegames/hat-api-gateway:latest
 docker stop hat-api-gateway
 docker system prune -f
-docker run -d -p 9002:9002 --env APP_HOST=172.17.0.1 --name=hat-api-gateway ghcr.io/itrolegames/hat-api-gateway:master
+docker run -d -p 9002:9002 --env APP_HOST=172.17.0.1 --name=hat-api-gateway ghcr.io/itrolegames/hat-api-gateway:latest
--- webhooks/scripts/redeploy-game.sh
+++ webhooks/scripts/redeploy-game.sh
@@ -2,4 +2,4 @@
 docker pull ghcr.io/itrolegames/hat-game-back:latest
 docker stop hat-game-back
 docker system prune -f
-docker run -d -p 9000:9000 --env APP_HOST=172.17.0.1 --env DB_PORT=5401 --env DB_USER=hat_user --env DB_PASSWORD=test1234 --name=hat-game-back ghcr.io/itrolegames/hat-game-back:master
+docker run -d -p 9000:9000 --env APP_HOST=172.17.0.1 --env DB_PORT=5401 --env DB_USER=hat_user --env DB_PASSWORD=test1234 --name=hat-game-back ghcr.io/itrolegames/hat-game-back:latest
